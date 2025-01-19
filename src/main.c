3611ecc2bd5abf7c0d851fe43fc960ccc78c5bc3
AeonianTest <s4006011@student.rmit.edu.au>
2025-01-20 00:03:50 +1100

    Adding core of main

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/*
+    Core of the program
+*/
+
+// Entry point to start running the program
+int main() {
+
+
+    return EXIT_SUCCESS; // TODO: HANDLE EXIT STATUS DYNAMICALLY
+}
+
+// The main function responsible for handling
+void shell_start() {
+
+}
---
+++ src/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Macros
+
+// Function declerations
+void shell_start(); // The main function responsible for handling user input
+
+#endif
