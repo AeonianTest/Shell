66a884a94ccc856f77164637a5d7d2686b097710
AeonianTest <s4006011@student.rmit.edu.au>
2025-01-21 00:08:01 +1100

    Working on the main input control loop

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "main.h"
 
 /*
@@ -7,12 +9,35 @@
 
 // Entry point to start running the program
 int main() {
-
-
+    shell_start(); // Start the shell
     return EXIT_SUCCESS; // TODO: HANDLE EXIT STATUS DYNAMICALLY
 }
 
-// The main function responsible for handling
+// The main function responsible for handling user input
 void shell_start() {
+    char input[BUFFER_SIZE];
 
+    // Always loop. Exits are manual breaks
+    while (1) {
+        // Display the shell prompt
+        printf("BACI Shell > ");
+        fflush(stdout); 
+
+        // Look for EOF or ctrl+d to exit the program
+        if (fgets(input, BUFFER_SIZE, stdin) == NULL) { // Read up to BUFFER_SIZE - 1 off a line
+            printf("\n\nExiting BACI. Have a pleasant day!\n"); // If EOF or ctrl+d, break
+            break;
+        }
+
+        // Remove newline character to make the input an acceptable string
+        input[strcspn(input, "\n")] = '\0';
+
+        // Exit condition. If true that input is exit is given, then quit
+        if (strcmp(input, "exit") == 0) {
+            printf("\nExiting BACI. Have a pleasant day!\n");
+            break;
+        }
+
+        //Execution of input would start here
+    }
 }
--- src/main.h
+++ src/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 
 // Macros
+#define BUFFER_SIZE 1024 //Buffer char size for input
 
 // Function declerations
 void shell_start(); // The main function responsible for handling user input
+void execute_inputs(); // TODO: Handle the execution of inputs. 
 
 #endif
