0f27ab065e3448a970c1b458f3cc1cbd6f4493ae
AeonianTest <s4006011@student.rmit.edu.au>
2025-01-21 20:37:25 +1100

    cat shell

---
+++ src/cat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "cat.h"
+
+/*
+    Implementaion of the unix cat command
+    To be compilied into the a signle executable with 
+*/
+
+void cat(char* filename) {
+
+}
---
+++ src/cat.h
@@ -0,0 +1,7 @@
+#ifndef CAT_H
+#define CAT_H
+
+// Function
+void cat(char* filename); // TODO: Print the contents of the file supplied
+
+#endif
