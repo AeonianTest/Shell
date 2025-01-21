007fcb54d5141ae5975b92e894103300dd4dc967
AeonianTest <s4006011@student.rmit.edu.au>
2025-01-21 19:32:51 +1100

    Tokenise the input line

--- src/main.c
+++ src/main.c
@@ -15,7 +15,8 @@ int main() {
 
 // The main function responsible for handling user input
 void shell_start() {
-    char input[BUFFER_SIZE];
+    char input[BUFFER_SIZE]; // Line of input
+    char *args[TOKENS_AMT]; // Arguments broken into seperate tokens
 
     // Always loop. Exits are manual breaks
     while (1) {
@@ -24,7 +25,7 @@ void shell_start() {
         fflush(stdout); 
 
         // Look for EOF or ctrl+d to exit the program
-        if (fgets(input, BUFFER_SIZE, stdin) == NULL) { // Read up to BUFFER_SIZE - 1 off a line
+        if (fgets(input, BUFFER_SIZE, stdin) == NULL) { // Read up to BUFFER_SIZE - 1 off a line, rewriting every new loop
             printf("\n\nExiting BACI. Have a pleasant day!\n"); // If EOF or ctrl+d, break
             break;
         }
@@ -32,12 +33,36 @@ void shell_start() {
         // Remove newline character to make the input an acceptable string
         input[strcspn(input, "\n")] = '\0';
 
-        // Exit condition. If true that input is exit is given, then quit
-        if (strcmp(input, "exit") == 0) {
+        // Exit commands. If true that input is exit or quit is given, then terminate the loop and thus program
+        if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
             printf("\nExiting BACI. Have a pleasant day!\n");
             break;
         }
 
-        //Execution of input would start here
+        // Parse the input here into tokens
+        tokenise_inputs(input, args);
+
+        if (args[0] != NULL) { //TESTING
+            printf("Command entered: %s\n", args[0]);
+        }
+
+        if (args[1] != NULL) { //TESTING
+            printf("Command entered: %s\n", args[1]);
+        }
     }
+}
+
+// Turn the arguments into tokens
+void tokenise_inputs(char *input, char **args) {
+    char *token = strtok(input, " \n\t");
+    int i = 0;
+    
+    // While tokens exist, keep appending
+    while (token != NULL && i < TOKENS_AMT - 1) {
+        args[i++] = token;
+        token = strtok(NULL, " \n\t");
+    }
+
+    // Null terminate the argument list
+    args[i] = NULL;  
 }
--- src/main.h
+++ src/main.h
@@ -2,10 +2,12 @@
 #define MAIN_H
 
 // Macros
-#define BUFFER_SIZE 1024 //Buffer char size for input
+#define BUFFER_SIZE 1024 // Buffer char size for input
+#define TOKENS_AMT 64 // Amount of tokens allowed in a line
 
 // Function declerations
 void shell_start(); // The main function responsible for handling user input
-void execute_inputs(); // TODO: Handle the execution of inputs. 
+void tokenise_inputs(char *input, char **args); // Turn a line of input into tokens
+void execute_inputs(char **args); // TODO: Handle the execution of the args (ie call the correct command and then ). 
 
 #endif
