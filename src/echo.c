c6273b74d9b94f3444bab1974f5af451aa1139f4
AeonianTest <s4006011@student.rmit.edu.au>
2025-01-25 16:30:22 +1100

    echo cmd shell

---
+++ src/echo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "echo.h"
+
+/*
+    Implementaion of the unix pwd command
+    Compilied with the final bin
+*/
+
+void echo(char **args) { // TODO: mvp implementation
+
+}
---
+++ src/echo.h
@@ -0,0 +1,7 @@
+#ifndef ECHO_H
+#define ECHO_H
+
+// Function
+void echo(char **args); // Write arguments to the standard output. TODO: Implementation
+
+#endif
