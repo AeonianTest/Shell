7bf2e6842ef545e8fdbd05824feeed9669a7aa54
AeonianTest <s4006011@student.rmit.edu.au>
2025-02-09 23:55:37 +1100

    ls cmd shell

---
+++ src/builtins/ls.c
@@ -0,0 +1,10 @@
+#include "ls.h"
+
+/*
+    Implementaion of the unix ls command
+    To be compilied into the a single executable with other source files
+*/
+
+void ls() { // TODO: mvp implementation of the list directory contents command
+    
+}
---
+++ src/builtins/ls.h
@@ -0,0 +1,7 @@
+#ifndef LS_H
+#define LS_H
+
+// Function
+void ls(); // TODO: mvp implementation of the list directory contents command
+
+#endif
