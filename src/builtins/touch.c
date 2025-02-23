fed591cfaa9b5b0adb7b63a928709d7117a06ded
AeonianTest <s4006011@student.rmit.edu.au>
2025-02-23 19:16:12 +1100

    touch cmd shell

---
+++ src/builtins/touch.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "touch.h"
+
+/*
+    Naive Implementaion of the unix touch command
+    To be compilied into the a single executable with other source files
+*/
+
+void touch(char **args) {
+    
+}
---
+++ src/builtins/touch.h
@@ -0,0 +1,10 @@
+#ifndef TOUCH_H
+#define TOUCH_H
+
+// Macros
+#define DEFAULT_PERMS 0644 // rw-r--r-- permission
+
+// Functions
+void touch(char **args); // Naive implementation of the file creation cmd touch
+
+#endif
