067274e28ee54c81d3e3a79f438ed935eb0e4300
AeonianTest <s4006011@student.rmit.edu.au>
2025-02-23 22:16:35 +1100

    Time stamps

--- src/builtins/touch.c
+++ src/builtins/touch.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <utime.h>
+#include <sys/stat.h>
 #include "touch.h"
 
 /*
@@ -9,16 +11,32 @@
 */
 
 void touch(char **args) {
-    // v0.1 bare min tm
+    // v0.2 bare min tm
+    struct stat fileStat;
+    int fileDesc;
 
-    // Open the file in write mode, if not exist
-    int fileDesc = open(args[1], STD_OPEN_FLAGS, DEFAULT_PERMS);
-    
-    // Basic err handling
-    if (fileDesc == -1) {
-        perror("touch");
-        return;
+    // Check if file exists, if true update access timestamps
+    if (stat(args[1], &fileStat) == 0) {
+        // File exists, update access time
+        if (utime(args[1], NULL) != 0) {
+            // If error occured in updating
+            perror("Error updating time stamps");
+        }
+        else {
+            printf("File %s already exists, updating timestamp\n", args[1]);
+        }
     }
+    else { // Else if 0 (Success) not returned by stat, file not exist so we can create it
+        // Open the file in write mode,
+        fileDesc = open(args[1], STD_OPEN_FLAGS, DEFAULT_PERMS);
+        
+        // Basic err handling
+        if (fileDesc < 0) {
+            perror("error creating file");
+            return;
+        }
 
-    close(fileDesc);
+        // Close file on use
+        close(fileDesc);
+    }
 }
