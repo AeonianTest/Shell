294dc962234d8bada4984447cb66740a981220cd
AeonianTest <s4006011@student.rmit.edu.au>
2025-02-24 21:57:19 +1100

    rm multi file handling

--- src/builtins/rm.c
+++ src/builtins/rm.c
@@ -10,22 +10,36 @@
 
 void rm(char **args) {
     struct stat fileStat;
+    int i = 0;
+    args += 1;
 
-    // Check if file exists
-    if (stat(args[1], &fileStat) != 0) {
-        perror("rm");
-        return; // rep w cont in final copy
-    }
-
-    // Bless stackoverflow
-    // Check if file is a directory
-    if (S_ISDIR(fileStat.st_mode)) {
-        perror("rm: cannot remove target, is a Directory");
+    if (args[0] == NULL) {
+        fprintf(stderr, "rm: missing file operand\n");
         return;
     }
 
-    // Basic unlinking of a file to free it up once no proccess' on it
-    if (unlink(args[1]) != 0) {
-        perror("rm: error when unlinking");
+    // Loop for all the arguments
+    while (args[i] != NULL) {
+        // Check if file exists
+        if (stat(args[i], &fileStat) != 0) {
+            perror("rm");
+            i++;
+            continue; // rep w cont in final copy
+        }
+
+        // Bless stackoverflow
+        // Check if file is a directory
+        if (S_ISDIR(fileStat.st_mode)) {
+            fprintf(stderr, "rm: cannot remove '%s': Is a directory\n", args[i]);
+            i++;
+            continue;
+        }
+
+        // Basic unlinking of a file to free it up once no proccess' on it
+        if (unlink(args[i]) != 0) {
+            perror("rm: error when unlinking");
+        }
+
+        i++;
     }
 }
